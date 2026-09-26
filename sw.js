43ee669d41b838e659d5427403caea7c4a2ce4ee
yuen yalung <yuen.yalung@gmail.com>
2026-09-26 20:23:34 +0800

    push

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '2.7.0'; // ← bump this on each deploy
+const VERSION = '2.8.0'; // ← bump this on each deploy
 const CACHE_NAME = `la-lyric-v${VERSION}`;
 const ASSETS_TO_CACHE = [
   './',
